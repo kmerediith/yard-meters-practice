1d3a3039512a2641def273178ca5596e0404ed4a
kmerediith <kymered24@gmail.com>
2023-10-04 00:28:06 -0400

    created a variable that displays the conversion of yards to meters

--- yards_meters.js
+++ yards_meters.js
@@ -1,23 +1,26 @@
 let inputYards = prompt("enter a number and we will convert it from yards to meters")
-console.log("Input Yards: ", inputYards)
+
 
 function convertToMeters(yards){
     return yards * 0.9144;
 }
 let convertedMeters = convertToMeters(inputYards);
-console.log("Converted Meters: ", convertedMeters)
+
 
 function createMessage(yards, meters){
     let message = '';
     const numYards = yards * 1;
 
     if (numYards === 1760){
-        message = "That is a long mile";
+        message = "That is a long mile"
     }else if (numYards === 100){
-        message = "That is as long as a football field"
+        message = "That is as long as a football field";
     }else if (numYards === 26){
-        message = "That is a long as a tennis court!"
+        message = "That is a long as a tennis court!";
     }
 
     return `${yards} yards is ${ meters} meters. ${message}`;
 }
+
+let output = createMessage(inputYards, convertedMeters);
+console.log(output);
