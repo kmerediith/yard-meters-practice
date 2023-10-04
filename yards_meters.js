e8f0cd414435e84c10c267d26329f4760e825f09
kmerediith <kymered24@gmail.com>
2023-10-03 20:18:45 -0400

    created inputYards variable and defined a convertToMeters function

---
+++ yards_meters.js
@@ -0,0 +1,6 @@
+let inputYards = prompt("enter a number and we will convert it from yards to meters")
+console.log("Input Yards: ", inputYards)
+
+function convertToMeters(yards){
+    return yards * 0.9144;
+}
